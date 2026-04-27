12c49f0d8814bc691dbba0582a72aac73eeb0851
5hau5 <chili.shaus@gmail.com>
2026-04-27 14:41:27 +0500

    some refactoring

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\Admin\GameListController;  
+use App\Http\Controllers\ProfileController; 
 use App\Http\Controllers\Admin\GameController;
 use App\Http\Controllers\Admin\GenreController;
 
@@ -14,19 +13,24 @@ Route::middleware("auth")->group(function () {
 
       //Route::get('gamelist',  [GameListController::class, 'index'])->name('gamelist');
 
-        Route::prefix('game') ->group(function () {
-            Route::get('list', [GameController::class,  'list'])->name('gamelist');
-            Route::get('{id}', [GameController::class, 'show'])->name('game.show');
-            Route::get('{id}/update', [GameController::class, 'edit'])->name('game.edit');
-            Route::post('{id}/update', [GameController::class, 'update'])->name('game.update');
-            Route::delete('{id}/delete', [GameController::class, 'destroy'])->name('game.delete');
+        Route::prefix('games')->group(function () {
+            Route::get('/', [GameController::class, 'list'])->name('games.index');    // List games
+            Route::get('create', [GameController::class, 'create'])->name('games.create');  // Show game creation form
+            Route::post('/', [GameController::class, 'store'])->name('games.store');  // Store new game
+            Route::get('{id}', [GameController::class, 'show'])->name('games.show');  // Show single game
+            Route::get('{id}/edit', [GameController::class, 'edit'])->name('games.edit');  // Show game edit form
+            Route::put('{id}', [GameController::class, 'update'])->name('games.update');  // Update game
+            Route::delete('{id}', [GameController::class, 'destroy'])->name('games.destroy');  // Delete game
         });
+
         Route::prefix('genres')->group(function () {
-            Route::get('', [GenreController::class, 'index'])->name('genres');
-            Route::get('{id}', [GenreController::class, 'show'])->name('genre.show');
-            Route::get('{id}/update', [GenreController::class, 'edit'])->name('genre.edit');
-            Route::post('{id}/update', [GenreController::class, 'update'])->name('genre.update');
-            Route::delete('{id}/delete', [GenreController::class, 'destroy'])->name('genre.delete');
+            Route::get('/', [GenreController::class, 'list'])->name('genres.index');  // List genres
+            Route::get('create', [GenreController::class, 'create'])->name('genres.create');  // Show genre creation form
+            Route::post('/', [GenreController::class, 'store'])->name('genres.store');  // Store new genre
+            Route::get('{id}', [GenreController::class, 'show'])->name('genres.show');  // Show single genre
+            Route::get('{id}/edit', [GenreController::class, 'edit'])->name('genres.edit');  // Show genre edit form
+            Route::put('{id}', [GenreController::class, 'update'])->name('genres.update');  // Update genre
+            Route::delete('{id}', [GenreController::class, 'destroy'])->name('genres.destroy');  // Delete genre
         });
 
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
