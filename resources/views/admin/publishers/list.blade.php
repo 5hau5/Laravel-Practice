9e21a63d7b8b4113616107d6b711a2a369f1709e
5hau5 <chili.shaus@gmail.com>
2026-05-06 09:14:12 +0500

    forgo to change the copied genre file into publisher format

--- resources/views/admin/publishers/list.blade.php
+++ resources/views/admin/publishers/list.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ 'Genre List' }}
+            {{ 'Publisher List' }}
         </h2>
     </x-slot>
 
@@ -9,22 +9,23 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ 'Genre List' }}
+                    {{ 'Publisher List' }}
                 </div>
                 <x-table.table 
-                :rows="$genres" 
+                :rows="$publishers" 
                 :fields="['id','name','description', 'actions']"
-                searchPlaceholder="Search Genres..." 
-                :searchRoute="route('genres.index')" 
-                :actions="['genres.show'=> 'View Details', 'genres.edit' => 'Edit', 'genres.destroy' => 'Delete']"
+                :headers="['#', 'Name', 'Description', 'Actions']"
+                searchPlaceholder="Search Publishers..." 
+                :searchRoute="route('publishers.index')" 
+                :actions="['publishers.show'=> 'View Details', 'publishers.edit' => 'Edit', 'publishers.destroy' => 'Delete']"
                 />
             </div>
         </div>
     </div>
 
     <div class="px-4 py-3">
-        <a href="{{ route('genres.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-wider hover:bg-gray-700 dark:hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-            {{ __('Create Genre') }}
+        <a href="{{ route('publishers.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-wider hover:bg-gray-700 dark:hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+            {{ __('Create Publisher') }}
         </a>
     </div>
 
